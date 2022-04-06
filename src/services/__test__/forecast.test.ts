0e9d04073ab3dc2959019498dab6480af78cae0f
Vinícius de Lima Silveira <viniciuslsilveiradev@gmail.com>
2022-04-06 17:00:26 -0300

    Using NOCK to mock tests

--- src/services/__test__/forecast.test.ts
+++ src/services/__test__/forecast.test.ts
@@ -1,11 +1,11 @@
 import { StormGlass } from '@src/clients/stormGlass';
 import stormGlassNormalizedResponseFixture from '@test/fixtures/stormglass_normalized_response_3_hours.json';
+import apiForecastResponse1BeachFixture from '@test/fixtures/api_forecast_response_1_beach.json';
 import {
     Forecast,
-    Beach,
-    BeachPosition,
     ForecastProcessingInternalError,
 } from '@src/services/forecast';
+import { Beach, BeachPosition } from '@src/models/Beach';
 
 jest.mock('@src/clients/stormGlass');
 
@@ -19,76 +19,15 @@ describe('Forecast Service', () => {
 
         const beaches: Beach[] = [
             {
+                _id: 'some-id',
                 lat: -33.792726,
                 lng: 151.289824,
                 name: 'Manly',
                 position: BeachPosition.E,
-                user: 'some-id',
             },
         ];
 
-        const expectedResponse = [
-            {
-                time: '2020-04-26T00:00:00+00:00',
-                forecast: [
-                    {
-                        lat: -33.792726,
-                        lng: 151.289824,
-                        name: 'Manly',
-                        position: 'E',
-                        rating: 1,
-                        swellDirection: 64.26,
-                        swellHeight: 0.15,
-                        swellPeriod: 3.89,
-                        time: '2020-04-26T00:00:00+00:00',
-                        waveDirection: 231.38,
-                        waveHeight: 0.47,
-                        windDirection: 299.45,
-                        windSpeed: 100,
-                    },
-                ],
-            },
-            {
-                time: '2020-04-26T01:00:00+00:00',
-                forecast: [
-                    {
-                        lat: -33.792726,
-                        lng: 151.289824,
-                        name: 'Manly',
-                        position: 'E',
-                        rating: 1,
-                        swellDirection: 123.41,
-                        swellHeight: 0.21,
-                        swellPeriod: 3.67,
-                        time: '2020-04-26T01:00:00+00:00',
-                        waveDirection: 232.12,
-                        waveHeight: 0.46,
-                        windDirection: 310.48,
-                        windSpeed: 100,
-                    },
-                ],
-            },
-            {
-                time: '2020-04-26T02:00:00+00:00',
-                forecast: [
-                    {
-                        lat: -33.792726,
-                        lng: 151.289824,
-                        name: 'Manly',
-                        position: 'E',
-                        rating: 1,
-                        swellDirection: 182.56,
-                        swellHeight: 0.28,
-                        swellPeriod: 3.44,
-                        time: '2020-04-26T02:00:00+00:00',
-                        waveDirection: 232.86,
-                        waveHeight: 0.46,
-                        windDirection: 321.5,
-                        windSpeed: 100,
-                    },
-                ],
-            },
-        ];
+        const expectedResponse = apiForecastResponse1BeachFixture;
 
         const forecast = new Forecast(mockedStormGlassService);
         const beachesWithRating = await forecast.processForecastForBeaches(
@@ -107,11 +46,11 @@ describe('Forecast Service', () => {
     it('should throw internal processing error when something goes wrong during the rating process', async () => {
         const beaches: Beach[] = [
             {
+                _id: 'some-id',
                 lat: -33.792726,
                 lng: 151.289824,
                 name: 'Manly',
                 position: BeachPosition.E,
-                user: 'some-id',
             },
         ];
 
