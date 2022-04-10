267e9bf56a0d8dcf4a349cbecde89c04a57cc5b5
Vinícius de Lima Silveira <viniciuslsilveiradev@gmail.com>
2022-04-10 12:57:37 -0300

    JWT authentication

--- src/controllers/users.ts
+++ src/controllers/users.ts
@@ -2,6 +2,7 @@ import { Controller, Post } from '@overnightjs/core';
 import { Request, Response } from 'express';
 import { User } from '@src/models/User';
 import { BaseController } from '.';
+import AuthService from '@src/services/auth';
 
 @Controller('users')
 export class UsersController extends BaseController {
@@ -14,5 +15,28 @@ export class UsersController extends BaseController {
         } catch (error: any) {
             this.sendCreateUpdateErrorResponse(res, error);
         }
+    }
+
+    @Post('authenticate')
+    public async authenticate(req: Request, res: Response): Promise<Response> {
+        const { email, password } = req.body;
+        const user = await User.findOne({ email });
+
+        if (!user) {
+            return res.status(401).send({
+                code: 401,
+                error: 'User not found!',
+            });
+        }
+        if (!(await AuthService.comparePasswords(password, user.password))) {
+            return res.status(401).send({
+                code: 401,
+                error: 'Password does not match!',
+            });
+        }
+
+        const token = AuthService.generateToken(user.toJSON());
+
+        return res.status(200).send({ token });
     }
 }
