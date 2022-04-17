d61b7285c3edeffb997c1e21097071551847d495
Vinícius de Lima Silveira <viniciuslsilveiradev@gmail.com>
2022-04-17 20:41:15 -0300

    Added frontend

---
+++ web/src/config.js
@@ -0,0 +1,3 @@
+// export const API_URL = 'https://typescript-surf-api.herokuapp.com/';
+export const API_URL = 'http://localhost:3333';
+export const GMAPS_API_KEY = 'AIzaSyBB854Viw6N2qgak_TT-C91kHKky82POYw';
