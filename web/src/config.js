5880f354a38ffee5390a709d17007a9be750ca2c
Vinícius de Lima Silveira <viniciuslsilveiradev@gmail.com>
2022-04-18 09:53:35 -0300

    Updated API_URL to point at heroku

--- web/src/config.js
+++ web/src/config.js
@@ -1,3 +1,2 @@
-export const API_URL = 'http://localhost:3333';
-// export const API_URL = 'https://typescript-surf-api.herokuapp.com';
+export const API_URL = 'https://typescript-surf-api.herokuapp.com';
 export const GMAPS_API_KEY = 'AIzaSyBB854Viw6N2qgak_TT-C91kHKky82POYw';
