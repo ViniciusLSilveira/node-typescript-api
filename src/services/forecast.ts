be62a9fa147ec8c0f5ee7124bf7ad354e16aff8b
Vinícius de Lima Silveira <viniciuslsilveiradev@gmail.com>
2022-04-17 22:25:53 -0300

    Rating beaches by rating

--- src/services/forecast.ts
+++ src/services/forecast.ts
@@ -2,6 +2,7 @@ import { ForecastPoint, StormGlass } from '@src/clients/stormGlass';
 import logger from '@src/logger';
 import { Beach } from '@src/models/Beach';
 import { InternalError } from '@src/util/errors/internalErrors';
+import _ from 'lodash';
 import { Rating } from './rating';
 export interface TimeForecast {
     time: string;
@@ -24,28 +25,38 @@ export class Forecast {
     public async processForecastForBeaches(
         beaches: Beach[]
     ): Promise<TimeForecast[]> {
-        const pointsWithCorrectSources: BeachForecast[] = [];
-        logger.info(`Preparing the forecast for ${beaches.length} beaches`);
         try {
-            for (const beach of beaches) {
-                const rating = new this.RatingService(beach);
-                const points = await this.stormGlass.fetchPoints(
-                    beach.lat,
-                    beach.lng
-                );
-                const enrichedBeachData = this.enrichedBeachData(
-                    points,
-                    beach,
-                    rating
-                );
-                pointsWithCorrectSources.push(...enrichedBeachData);
-            }
-
-            return this.mapForecastByTime(pointsWithCorrectSources);
+            const beachForecast = await this.calculateRating(beaches);
+            const timeForecast = this.mapForecastByTime(beachForecast);
+            return timeForecast.map((t) => ({
+                time: t.time,
+                // TODO Allow ordering to be dynamic Sorts the beaches by its ratings
+                forecast: _.orderBy(t.forecast, ['rating'], ['desc']),
+            }));
         } catch (error: any) {
             logger.error(error);
             throw new ForecastProcessingInternalError(error.message);
         }
+    }
+
+    private async calculateRating(beaches: Beach[]): Promise<BeachForecast[]> {
+        const pointsWithCorrectSources: BeachForecast[] = [];
+        logger.info(`Preparing the forecast for ${beaches.length} beaches`);
+        for (const beach of beaches) {
+            const rating = new this.RatingService(beach);
+            const points = await this.stormGlass.fetchPoints(
+                beach.lat,
+                beach.lng
+            );
+            const enrichedBeachData = this.enrichedBeachData(
+                points,
+                beach,
+                rating
+            );
+            pointsWithCorrectSources.push(...enrichedBeachData);
+        }
+
+        return pointsWithCorrectSources;
     }
 
     private enrichedBeachData(
